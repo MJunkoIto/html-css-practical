c824644f686a941fbd7ddfc0e41e2cb2ed569ab8
MJunkoIto <soutozen2018@gmail.com>
2025-04-12 07:30:55 +0900

    0412 responsive constructing

---
+++ portforio/css/script.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('.page-top');
+
+button.addEventListener('click', () => {
+  window.scroll({ 
+    top: 0, 
+    behavior: "smooth"
+  });
+});
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 100){
+    button.classList.add('is-active');
+  }else{
+    button.classList.remove('is-active');
+  }
+});
